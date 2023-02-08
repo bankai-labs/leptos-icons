f38f8f89511afc40f54947906e9305a37c15c0c7
b4-io <brunodesimone17@gmail.com>
2023-02-08 19:29:51 -0300

    fix width height

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,10 @@ pub fn Icon(
         IconType::HeroIcons(HeroType) => match HeroType {
             HeroIconsType::Outline => {
                 view! {cx,
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class() width=width() height=height() viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class=class() viewBox="0 0 24 24"
                         data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
+                        width=move || width().is_empty().then_some("24".to_string()).unwrap_or(width())
+                        height=move || height().is_empty().then_some("24".to_string()).unwrap_or(height())
                         fill=move || fill().is_empty().then_some("none".to_string()).unwrap_or(fill())
                         stroke-width=move || stroke_width().is_empty().then_some("2".to_string()).unwrap_or(stroke_width())
                         stroke=move || stroke().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke())
@@ -66,8 +68,10 @@ pub fn Icon(
             }
             HeroIconsType::Solid => {
                 view! {cx,
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class() width=width() height=height() viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class=class() viewBox="0 0 24 24"
                         data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
+                        width=move || width().is_empty().then_some("24".to_string()).unwrap_or(width())
+                        height=move || height().is_empty().then_some("24".to_string()).unwrap_or(height())
                         fill=move || fill().is_empty().then_some("currentColor".to_string()).unwrap_or(fill())
                         stroke-width=move || stroke_width().is_empty().then_some("2".to_string()).unwrap_or(stroke_width()) stroke=move || stroke().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke())
                     >
@@ -77,8 +81,10 @@ pub fn Icon(
             }
             HeroIconsType::Mini => {
                 view! {cx,
-                    <svg xmlns="http://www.w3.org/2000/svg" class=class() width=width() height=height() viewBox="0 0 20 20"
+                    <svg xmlns="http://www.w3.org/2000/svg" class=class() viewBox="0 0 20 20"
                         data-license="From https://github.com/tailwindlabs/heroicons - Licensed under MIT"
+                        width=move || width().is_empty().then_some("20".to_string()).unwrap_or(width())
+                        height=move || height().is_empty().then_some("20".to_string()).unwrap_or(height())
                         fill=move || fill().is_empty().then_some("currentColor".to_string()).unwrap_or(fill())
                         stroke-width=move || stroke_width().is_empty().then_some("2".to_string()).unwrap_or(stroke_width()) stroke=move || stroke().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke())
                     >
@@ -90,8 +96,10 @@ pub fn Icon(
         },
         IconType::Lucide => {
             view! {cx,
-                <svg xmlns="http://www.w3.org/2000/svg" class=class() width=width() height=height() viewBox="0 0 24 24"
+                <svg xmlns="http://www.w3.org/2000/svg" class=class() viewBox="0 0 24 24"
                     data-license="From https://github.com/lucide-icons/lucide - Licensed under ISC"
+                        width=move || width().is_empty().then_some("24".to_string()).unwrap_or(width())
+                        height=move || height().is_empty().then_some("24".to_string()).unwrap_or(height())
                     fill=move || fill().is_empty().then_some("none".to_string()).unwrap_or(fill())
                     stroke-width=move || stroke_width().is_empty().then_some("2".to_string()).unwrap_or(stroke_width()) stroke=move || stroke().is_empty().then_some("currentColor".to_string()).unwrap_or(stroke()) 
                     stroke-linecap="round" stroke-linejoin="round"
